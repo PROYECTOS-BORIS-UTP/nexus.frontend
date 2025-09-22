c9fc8339068f058fe40c420c2363e2e9235ff301
IsaacLCH12 <isaaclivaque123@gmail.com>
2025-09-22 00:09:34 -0500

    avanceTablaUsuarios1
    le meti la tabla pero aun falta diseños y entre otras cosas

--- src/app/modules/configuracion/seguridad/usuario/pages/usuario-page/usuario-page.ts
+++ src/app/modules/configuracion/seguridad/usuario/pages/usuario-page/usuario-page.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+export interface Usuario {
+  idUsuario: number;
+  vUsuario: string;
+  bActivo: boolean;
+  idTipoUsuario: number;
+  idPersona: number | null;
+  idTipoPersona: number;
+  bChangePassword: boolean;
+}
+
+const ELEMENT_DATA: Usuario[] = [
+  {
+    idUsuario: 1,
+    vUsuario: 'bestradas',
+    bActivo: true,
+    idTipoUsuario: 1,
+    idPersona: null,
+    idTipoPersona: 1,
+    bChangePassword: false,
+  },
+  {
+    idUsuario: 2,
+    vUsuario: 'admin',
+    bActivo: true,
+    idTipoUsuario: 2,
+    idPersona: 5,
+    idTipoPersona: 1,
+    bChangePassword: true,
+  },
+];
 
 @Component({
   selector: 'app-usuario-page',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './usuario-page.html',
-  styleUrl: './usuario-page.scss'
+  styleUrl: './usuario-page.scss',
 })
 export class UsuarioPage {
+  displayedColumns: string[] = [
+    'idUsuario',
+    'vUsuario',
+    'bActivo',
+    'idTipoUsuario',
+    'idPersona',
+    'idTipoPersona',
+    'bChangePassword',
+  ];
+  dataSource = ELEMENT_DATA;
 
+  clickedRows = new Set<Usuario>();
+
+  toggleRow(row: Usuario) {
+    if (this.clickedRows.has(row)) {
+      this.clickedRows.delete(row); //  Quita si ya estaba seleccionado
+    } else {
+      this.clickedRows.add(row); // Agrega si no estaba
+    }
+  }
 }
