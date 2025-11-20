a8de5ce1d3e4a5efdb0e8025ba02eb659a983c8f
IsaacLCH12 <isaaclivaque123@gmail.com>
2025-11-20 17:35:32 -0500

    pais parte 1

---
+++ src/app/modules/configuracion/maestras/pais/pages/pais-page/dialogs/pais-form/pais-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-form',
+  imports: [],
+  templateUrl: './pais-form.html',
+  styleUrl: './pais-form.scss'
+})
+export class PaisForm {
+
+}
--- src/app/modules/configuracion/maestras/pais/services/pais.service.ts
+++ src/app/modules/configuracion/maestras/pais/services/pais.service.ts
@@ -6,6 +6,10 @@ import { IPaisResponse } from '../interfaces/response/IPaisResponse.interface';
 import { environment } from '../../../../../../environments/environments';
 import { IApiResponse } from '../../../../../core/interfaces/IApiResponse.interface';
 import { IPaginationResponse } from '../../../../../core/interfaces/IPaginationResponse.interface';
+import { IPaisCreateUpdateResponse } from '../interfaces/response/IPaisCreateUpdateResponse.interface';
+import { IPaisCreateUpdateRequest } from '../interfaces/request/IPaisCreateUpdateRequest.interface';
+import { handleHttpError } from '../../../../../core/utils/error-handler.utils';
+import { IPaisDeleteResponse } from '../interfaces/response/IPaisDeleteResponse.interface';
 
 
 @Injectable({
@@ -33,11 +37,55 @@ export class PaisService {
                     throw new Error(response.vMessage || 'Error desconocido al obtener países');
                 }
             }),
-            catchError(error => {
-                console.error('Error en la llamada HTTP a listarPaises:', error);
-                const errorMessage = error?.error?.vMessage || error?.message || 'Error del servidor al listar países';
-                return throwError(() => new Error(errorMessage));
-            })
+            catchError(handleHttpError)
         );
     }
+
+    /*
+         * Envía una solicitud para crear o actualizar una Pais.
+         * @param request DTO con los datos de la Pais.
+         * @returns Un Observable con la respuesta del backend.
+         */
+        crearActualizarCompania(request: IPaisCreateUpdateRequest): Observable<IPaisCreateUpdateResponse> {
+            const url = `${this.apiUrl}/CrearActualizarPais`;
+            return this.http.post<IApiResponse<IPaisCreateUpdateResponse>>(url, request).pipe(
+                map(response => {                
+                    if (response && typeof response.bStatus === 'boolean') {
+                        if (response.bStatus) {
+                            return response.aData;
+                        } else {
+                            throw new Error(response.vMessage || 'El backend indicó un error al crear/actualizar la Pais.');
+                        }
+                    } else {
+                        console.error('Respuesta inesperada del backend:', response);
+                        throw new Error('Respuesta inesperada del servidor al crear/actualizar.');
+                    }
+                }),
+                catchError(handleHttpError)
+            );
+        }
+    
+        /*
+         * Envía una solicitud para eliminar (baja lógica) una Pais.
+         * @param iIdPais ID de la Pais a eliminar.
+         * @returns Un Observable con la respuesta del backend.
+         */
+        eliminarCompania(iIdPais: number): Observable<IPaisDeleteResponse> {
+            const url = `${this.apiUrl}/EliminarPais/${iIdPais}`;
+            return this.http.delete<IApiResponse<IPaisDeleteResponse>>(url).pipe(
+                map(response => {
+                    if (response && typeof response.bStatus === 'boolean') {
+                        if (response.bStatus) {
+                            return response.aData;
+                        } else {
+                            throw new Error(response.vMessage || 'El backend indicó un error al eliminar la Pais.');
+                        }
+                    } else {
+                        console.error('Respuesta inesperada del backend:', response);
+                        throw new Error('Respuesta inesperada del servidor al eliminar.');
+                    }
+                }),
+                catchError(handleHttpError)
+            );
+        }
 }
