3629247cf31aeef00837399ffd92b4bbb4ca338a
Bstrada4 <bstrada4@gmail.com>
2025-12-19 18:11:30 -0500

    upd boris

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true, // <--- Importante para optimizar
+    UrlBase: 'http://192.168.100.23:3000/api', // <--- Tu IP del Servidor
+    UrlFileBase: 'http://192.168.100.23:3000/',
+    key_ApiManagement: '',
+    secretKeyEncript: 'D1x3rIvQpWn0bA5J'
+};
